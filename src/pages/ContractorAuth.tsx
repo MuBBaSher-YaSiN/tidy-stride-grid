3ef6cb1459c178eda61763b68a1007f4ddb05bd6
Fatima_Iqra <iqraf683@gmail.com>
2025-09-02 13:26:52 +0500

    add comments

--- src/pages/ContractorAuth.tsx
+++ src/pages/ContractorAuth.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ArrowLeft, User, Mail, Lock } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
-
+//adding conmments
 const ContractorAuth = () => {
   const [formData, setFormData] = useState({
     email: "",
